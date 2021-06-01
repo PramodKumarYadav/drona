0aa07f7285b0b84997c6cfc384904c4e63377b2f
Bookanakere <rash.pb@gmail.com>
2021-06-01 08:24:55 +0530

    removed dependency on chromedriver exe.

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,5 @@
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -14,6 +16,7 @@ class LoginTest {
 
     @Test
     void test()  {
+        WebDriverManager.getInstance(CHROME).setup();
         WebDriver driver = new ChromeDriver();
         String AppURL = "https://www.saucedemo.com/";
         driver.get(AppURL);
