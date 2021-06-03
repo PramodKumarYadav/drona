53a177676aca5f86fd5fffc121dacf5fb86a95a0
Bookanakere <rash.pb@gmail.com>
2021-06-03 21:45:46 +0530

    Added tests for 4 users (#27)
    * Added tests for 4 users
    
    * added all LoginTests under Login class
    
    Co-authored-by: Pramod Kumar Yadav <pramodyadav027@gmail.com>

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,5 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,13 +11,15 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class LoginTest {
 
-    @Test
-    void test() {
+    @ParameterizedTest
+    @CsvSource({"standard_user,secret_sauce"
+            , "problem_user,secret_sauce"
+    })
+    void assertThatAValidUserCanLogIn(String userName, String password) {
         WebDriverManager.getInstance(CHROME).setup();
         WebDriver driver = new ChromeDriver();
 
@@ -23,9 +27,9 @@ class LoginTest {
         driver.get(AppURL);
 
         WebElement username = driver.findElement(By.id("user-name"));
-        username.sendKeys("standard_user");
-        WebElement password = driver.findElement(By.id("password"));
-        password.sendKeys("secret_sauce");
+        username.sendKeys(userName);
+        WebElement pwd = driver.findElement(By.id("password"));
+        pwd.sendKeys(password);
         WebElement submitButton = driver.findElement(By.id("login-button"));
         submitButton.click();
 
@@ -37,7 +41,51 @@ class LoginTest {
     }
 
     @Test
-    void thisIsAOneLinerTest() {
-        assertTrue(true);
+    void assertThatALockedOutUserCanNotLogIn() {
+        WebDriverManager.getInstance(CHROME).setup();
+        WebDriver driver = new ChromeDriver();
+
+        String AppURL = "https://www.saucedemo.com/";
+        driver.get(AppURL);
+
+        WebElement username = driver.findElement(By.id("user-name"));
+        username.sendKeys("locked_out_user");
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("secret_sauce");
+        WebElement submitButton = driver.findElement(By.id("login-button"));
+        submitButton.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        
+        WebElement error = driver.findElement(By.xpath("//h3[@data-test='error']"));
+        System.out.println(error.getText());
+
+        Boolean titleElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h3[@data-test='error']"))).isDisplayed();
+        assertEquals(titleElement, true);
+
+        driver.quit();
     }
+
+    @Test
+    void assertThatAPerformanceIssueUserCanLogInButAfterSomeDelays() {
+        WebDriverManager.getInstance(CHROME).setup();
+        WebDriver driver = new ChromeDriver();
+
+        String AppURL = "https://www.saucedemo.com/";
+        driver.get(AppURL);
+
+        WebElement username = driver.findElement(By.id("user-name"));
+        username.sendKeys("performance_glitch_user");
+        WebElement pwd = driver.findElement(By.id("password"));
+        pwd.sendKeys("secret_sauce");
+        WebElement submitButton = driver.findElement(By.id("login-button"));
+        submitButton.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement titleElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@class = 'title']")));
+        assertEquals(titleElement.getAttribute(("innerHTML")), "Products");
+
+        driver.quit();
+    }
+
 }
