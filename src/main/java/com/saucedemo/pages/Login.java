03e99f558b6f0a2bbf7addf17be9796713fa77d0
Bookanakere <rash.pb@gmail.com>
2021-06-08 22:16:53 +0530

    introduced POM and Page Factory (#32)
    * introduced POM and Page Factory
    
    * small config changes
    
    * added test for performance glitch user
    
    * deleted old logintest class
    
    * class, method, variables renamed to follow convention
    
    * removed variables used only once
    
    * moved login instantiation to before
    
    * moved driver.get to login method
    
    * changed instance name to login
    
    * few changes based on comments for PR

---
+++ src/main/java/com/saucedemo/pages/Login.java
@@ -0,0 +1,60 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Login {
+    private WebDriver driver;
+
+    @FindBy(name = "user-name")
+    private WebElement username;
+
+    @FindBy(name = "password")
+    private WebElement password;
+
+    @FindBy(name = "login-button")
+    private WebElement login;
+
+    @FindBy(className = "title")
+    private WebElement title;
+
+    @FindBy(xpath = "//h3[@data-test='error']")
+    private WebElement lockOutError;
+
+    public Login(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    private void setUsername(String user) {
+        username.sendKeys(user);
+    }
+
+    private void setPassword(String password) {
+        this.password.sendKeys(password);
+    }
+
+    private void clickLogin() {
+        login.click();
+    }
+
+    public String getTitle() {
+        return title.getAttribute("innerHTML");
+    }
+
+    public String getLoginLockOutError() {
+        return lockOutError.getText();
+    }
+
+    public void login(String user, String password) {
+
+        driver.get("https://www.saucedemo.com/");
+        setUsername(user);
+        setPassword(password);
+        clickLogin();
+    }
+
+}
+
---
+++ src/test/java/com/saucedemo/tests/UglyLoginTest.java
@@ -0,0 +1,55 @@
+package com.saucedemo.tests;
+
+import com.saucedemo.pages.Login;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UglyLoginTest {
+
+    private WebDriver driver;
+    com.saucedemo.pages.Login login;
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.getInstance(CHROME).setup();
+
+        driver = new ChromeDriver();
+        login = new Login(driver);
+    }
+
+    @AfterEach
+    public void teardown() {
+        driver.quit();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"standard_user,secret_sauce",
+            "problem_user,secret_sauce"
+    })
+
+    public void assertThatAValidUserCanLogin(String username, String password) {
+        login.login(username, password);
+        assertEquals("Products", login.getTitle());
+    }
+
+    @Test
+    public void assertThatALockedOutUserCanNotLogIn() {
+        login.login("locked_out_user", "secret_sauce");
+        assertEquals("Epic sadface: Sorry, this user has been locked out.", login.getLoginLockOutError());
+    }
+
+    @Test
+    public void assertThatAPerformanceIssueUserCanLogInButAfterSomeDelays() {
+        login.login("performance_glitch_user", "secret_sauce");
+        assertEquals("Products", login.getTitle());
+    }
+}
