876fc90fc824c3d5dc6a8ee237c8cacca8541832
Bookanakere <rash.pb@gmail.com>
2021-06-20 21:33:24 +0530

    chrome options update

--- src/main/java/com/saucedemo/pages/DriverFactory.java
+++ src/main/java/com/saucedemo/pages/DriverFactory.java
@@ -3,6 +3,7 @@ package com.saucedemo.pages;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
 import static io.github.bonigarcia.wdm.DriverManagerType.FIREFOX;
@@ -15,7 +16,11 @@ public class DriverFactory {
         switch (browser) {
             case "chrome":
                 WebDriverManager.getInstance(CHROME).setup();
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--headless");
+                driver = new ChromeDriver(options);
                 break;
 
             case "firefox":
